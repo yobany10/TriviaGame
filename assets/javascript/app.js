33d4ecc770c3881f6220e2e68c401ed2630106d2
Yobany <yobanydesign@gmail.com>
2018-10-29 17:32:17 -0400

    added trivia questions array

---
+++ assets/javascript/app.js
@@ -0,0 +1,42 @@
+var triviaQuestions = [{
+    question: "How long does it take for sun rays to reach Earth?",
+    answerList: ["1", "2", "3", "4"],
+    answer: 0
+},{
+    question: "How long does it take for sun rays to reach Earth?",
+    answerList: ["1", "2", "3", "4"],
+    answer: 0
+},{
+    question: "How long does it take for sun rays to reach Earth?",
+    answerList: ["1", "2", "3", "4"],
+    answer: 0
+},{
+    question: "How long does it take for sun rays to reach Earth?",
+    answerList: ["1", "2", "3", "4"],
+    answer: 0
+},{
+    question: "How long does it take for sun rays to reach Earth?",
+    answerList: ["1", "2", "3", "4"],
+    answer: 0
+},{
+    question: "How long does it take for sun rays to reach Earth?",
+    answerList: ["1", "2", "3", "4"],
+    answer: 0
+},{
+    question: "How long does it take for sun rays to reach Earth?",
+    answerList: ["1", "2", "3", "4"],
+    answer: 0
+},{
+    question: "How long does it take for sun rays to reach Earth?",
+    answerList: ["1", "2", "3", "4"],
+    answer: 0
+},{
+    question: "How long does it take for sun rays to reach Earth?",
+    answerList: ["1", "2", "3", "4"],
+    answer: 0
+},{
+    question: "How long does it take for sun rays to reach Earth?",
+    answerList: ["1", "2", "3", "4"],
+    answer: 0
+}];
+
