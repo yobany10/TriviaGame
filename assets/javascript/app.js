bd1116ff5d7f629361b5f53bd70ff92d45102ed2
Yobany <yobanydesign@gmail.com>
2018-10-29 18:39:57 -0400

    added messages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -41,4 +41,15 @@ var triviaQuestions = [{
 }];
 
 var gifArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10'];
-var currentQuestion
+var currentQuestion; var correctAnswer; var incorrectAnswer; var unanswered; var seconds; var time; var answered; var userSelect;
+var messages = {
+    correct: "Correct!",
+    incorrect: "Incorrect!",
+    endTime: "Out of time!",
+    finished: "Let's see how well you did."
+}
+
+$('#startButton').on('click', function(){
+    $(this).hide();
+    newGame();
+});
