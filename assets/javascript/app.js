a759ce2fce4e835d0d4d0cb5ee71bb7f0da82e35
Yobany <yobanydesign@gmail.com>
2018-10-29 18:24:58 -0400

    added to javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -40,3 +40,5 @@ var triviaQuestions = [{
     answer: 0
 }];
 
+var gifArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10'];
+var currentQuestion
